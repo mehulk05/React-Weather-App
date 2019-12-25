6dfd9ab01b27ba29f8db605731b227f4fd38d80a
mehulk05 <mehulkothari05@gmail.com>
2019-12-25 11:43:30 +0530

    first commit

---
+++ dist/precache-manifest.babf8bbb57055234be2bd3e3905c9afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281df170d2b5fe4c44fecaaff3436f4b",
+    "url": "/Weather-Web-App/index.html"
+  },
+  {
+    "revision": "ae56f8dd2793e321b197",
+    "url": "/Weather-Web-App/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "7839ecd9abd1277d6de4",
+    "url": "/Weather-Web-App/static/css/main.245ea75d.chunk.css"
+  },
+  {
+    "revision": "ae56f8dd2793e321b197",
+    "url": "/Weather-Web-App/static/js/2.415987a4.chunk.js"
+  },
+  {
+    "revision": "7839ecd9abd1277d6de4",
+    "url": "/Weather-Web-App/static/js/main.ee13be2c.chunk.js"
+  },
+  {
+    "revision": "98deb1221abbdcbffe4d",
+    "url": "/Weather-Web-App/static/js/runtime~main.40d86416.js"
+  }
+]);
---
+++ src/component/Titles.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container">Know Weather</h1>
+  </div>
+);
+
+export default Titles;
